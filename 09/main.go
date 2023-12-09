802d8a5e09053a3c07b295940158902938d1c6e7
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2023-12-09 19:28:25 +0100

    [fix] day 9 bench

--- 09/main.go
+++ 09/main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	fmt.Println("Part One :")
-	debut := time.Now()
-	fmt.Println("Temps d'exécution : ", time.Since(debut))
+	debut1 := time.Now()
 	fmt.Println("Result : ", part_one.Part_One())
+	fmt.Println("Temps d'exécution : ", time.Since(debut1))
 	fmt.Println("Part Two :")
-	debut = time.Now()
+	debut2 := time.Now()
 	fmt.Println("Result : ", part_two.Part_Two())
-	fmt.Println("Temps d'exécution : ", time.Since(debut))
+	fmt.Println("Temps d'exécution : ", time.Since(debut2))
 
 }
